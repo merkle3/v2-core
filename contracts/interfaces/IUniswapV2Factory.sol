0da53c35be0747cf6ce0341d9168c6975d9d50fa
Luca Provini <luca.provini@usemerkle.com>
2024-05-23 13:12:21 +0200

    fork

--- contracts/interfaces/IUniswapV2Factory.sol
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -9,6 +9,7 @@ interface IUniswapV2Factory {
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint256) external view returns (address pair);
     function allPairsLength() external view returns (uint256);
+    function pairCodeHash() external pure returns (bytes32);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
